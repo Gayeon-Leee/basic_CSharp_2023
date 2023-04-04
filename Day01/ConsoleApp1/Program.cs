80f1260d9bb4ed0fd00a31c7e04e55dea27131f3
Gayeon_Lee913 <gy9411@naver.com>
2023-04-04 12:13:12 +0900

    C# 첫번째 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 네임스페이스 ConsoleApp1 ConsoleApp1
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 프로그램 클래스
+    /// </summary>
+    internal class Program  // 파일명이랑 클래스명은 다르게 쓸 수도 있지만 관리 용이성 위해서는 같은 이름 쓰는게 좋음
+    {
+        /// <summary>
+        /// 메인 엔트리 포인트
+        /// </summary>
+        /// <param name="args">콘솔 매개변수</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!!");
+        }
+    }
+}
