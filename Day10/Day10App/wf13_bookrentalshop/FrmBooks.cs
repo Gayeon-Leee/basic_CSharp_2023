945178ebd0f879bdc2ea42fef2eba7cd06afa662
Gayeon_Lee913 <gy9411@naver.com>
2023-04-17 13:43:50 +0900

    4/17 완료

---
+++ Day10/Day10App/wf13_bookrentalshop/FrmBooks.cs
@@ -0,0 +1,273 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace wf13_bookrentalshop
+{
+    public partial class FrmBooks
+        : Form
+    {
+        bool isNew = false; // false(UPDATE) / true(INSERT)
+
+        #region < 생성자 >
+        public FrmBooks() // 생성자는 return타입(void...) 없음
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region < 이벤트 핸들러 >
+        private void FrmGenre_Load(object sender, EventArgs e)
+        {
+            isNew = true; // 신규부터 시작
+            RefreshData();
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if (CheckValidation() != true) return; // Validation true 아니면 저장안되게 하는 것
+
+            SaveData(); // 데이터 저장/수정            
+            RefreshData(); // 데이터 재조회
+            ClearInputs(); // 입력창 클리어
+        }
+
+        private void BtnDel_Click(object sender, EventArgs e)
+        {
+            if (isNew == true)  // 신규
+            {
+                MessageBox.Show("삭제할 데이터를 선택하세요", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+            using (MySqlConnection conn = new MySqlConnection(Helpers.Commons.ConnString))
+            // FK제약조건으로 지울 수 없는 데이터인지 먼저 확인
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                string strChkQuery = "SELECT COUNT(*) FROM bookstbl WHERE Division = @Divison";
+                MySqlCommand chkCmd = new MySqlCommand(strChkQuery, conn);
+                MySqlParameter prmDivision = new MySqlParameter("@Divison", TxtBookIdx.Text);
+                chkCmd.Parameters.Add(prmDivision);
+
+                var result = chkCmd.ExecuteScalar();
+
+                if (result.ToString() != "0")
+                {
+                    MessageBox.Show("이미 사용중인 코드입니다.", "삭제", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+
+            if (MessageBox.Show(this, "삭제하시겠습니까?", "삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
+            // 삭제 여부를 물을 때 아니오를 누르면 삭제 진행 취소
+
+            DeleteData(); // 데이터 삭제처리
+            RefreshData();
+            ClearInputs();
+        }
+
+        private void DgvResult_CellClick(object sender, DataGridViewCellEventArgs e) // 그리드뷰 클릭하면 발생하는 이벤트
+        {
+            if (e.RowIndex > -1) // 아무 것도 선택 안하면 -1(인덱스 0부터 시작하기 때문에)
+            {
+                var selData = DgvResult.Rows[e.RowIndex];
+
+                // Debug.WriteLine(selData.ToString()); 디버그창에서 확인
+                Debug.WriteLine(selData.Cells[0].Value);
+                Debug.WriteLine(selData.Cells[1].Value);
+                TxtBookIdx.Text = selData.Cells[0].Value.ToString();
+                TxtAuthor.Text = selData.Cells[1].Value.ToString();
+                TxtBookIdx.ReadOnly = true; // PK는 수정하면 안됨!!
+
+                isNew = false; // 수정
+            }
+        }
+
+        #endregion
+
+        #region < 커스텀 메서드 >
+
+        private void ClearInputs()
+        {
+            TxtBookIdx.Text = TxtAuthor.Text = string.Empty;
+            TxtBookIdx.ReadOnly = false; // 신규일때는 division 입력할 수 있어야해서 ReadOnly false
+            TxtBookIdx.Focus();
+            isNew = true; // 신규
+        }
+
+        private bool CheckValidation() // 입력검증(Validation check)
+        {
+            var result = true;
+            var errorMsg = string.Empty;
+
+            if (string.IsNullOrEmpty(TxtBookIdx.Text))
+            {
+                result = false;
+                errorMsg = "● 장르코드를 입력하세요.\r\n";
+            }
+
+            if (string.IsNullOrEmpty(TxtAuthor.Text))
+            {
+                result = false;
+                errorMsg += "● 장르명을 입력하세요.\r\n";
+            }
+
+            if (result == false)
+            {
+                MessageBox.Show(errorMsg, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return result;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        private void RefreshData()
+        {
+            //DB divtbl 데이터 조회 - DgvResult에 뿌림
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Helpers.Commons.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    // 쿼리 작성
+                    var selQuery = @"SELECT b.bookIdx,
+	                                        b.Author,
+                                            b.Division,
+                                            d.Names AS DivNames,
+                                            b.Names,
+                                            b.ReleaseDate,
+                                            b.ISBN,
+                                            b.Price
+                                       FROM bookstbl AS b
+                                      INNER JOIN divtbl as d
+                                         ON b.Division = d.Division
+                                      ORDER BY b.bookIdx ASC";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(selQuery, conn);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "bookstbl"); // bookstbl DataSet 접근 가능
+
+                    DgvResult.DataSource = ds.Tables[0];
+                    
+
+                    DgvResult.Columns[0].HeaderText = "번호";
+                    DgvResult.Columns[1].HeaderText = "저자명";
+                    DgvResult.Columns[2].HeaderText = "책장르";
+                    DgvResult.Columns[3].HeaderText = "책장르";
+                    DgvResult.Columns[4].HeaderText = "책제목";
+                    DgvResult.Columns[5].HeaderText = "출판일자";
+                    DgvResult.Columns[6].HeaderText = "ISBN";
+                    DgvResult.Columns[7].HeaderText = "책가격";
+
+                    DgvResult.Columns[0].Width = 55;
+                    DgvResult.Columns[2].Visible = false; // B001 같은 코드 영역은 사용자 화면에서 볼 필요 없음
+                    DgvResult.Columns[5].Width = 78;
+                    DgvResult.Columns[7].Width = 80;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"비정상적 오류 : {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Helpers.Commons.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    var query = "";
+
+                    if (isNew)
+                    {
+                        query = @"INSERT INTO divtbl
+                                       VALUES (@Division, @Names)";
+                    }
+                    else
+                    {
+                        query = @"UPDATE divtbl
+                                     SET Names = @Names
+                                   WHERE Division = @Division";
+                    }
+
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlParameter prmDivision = new MySqlParameter("@Division", TxtBookIdx.Text);
+                    MySqlParameter prmNames = new MySqlParameter("@Names", TxtAuthor.Text);
+                    cmd.Parameters.Add(prmDivision);
+                    cmd.Parameters.Add(prmNames);
+
+                    var result = cmd.ExecuteNonQuery(); // INSERT, UPDATE, DELETE
+
+                    if (result != 0)
+                    {
+                        // 저장성공
+                        MessageBox.Show("저장성공!", "저장", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        // 저장실패
+                        MessageBox.Show("저장실패!", "저장", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"비정상적 오류 : {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DeleteData()
+        {
+            try // Yes 누르면 삭제 진행 - SaveData()에 있는 로직 복사 후 수정한 코드임
+            {
+                using (MySqlConnection conn = new MySqlConnection(Helpers.Commons.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    var query = @"DELETE FROM divtbl
+                                        WHERE Division = @Division";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlParameter prmDivision = new MySqlParameter("@Division", TxtBookIdx.Text);
+                    cmd.Parameters.Add(prmDivision);
+
+                    var result = cmd.ExecuteNonQuery(); // INSERT, UPDATE, DELETE
+
+                    if (result != 0)
+                    {
+                        // 삭제성공
+                        MessageBox.Show("삭제성공!", "삭제", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        // 삭제실패
+                        MessageBox.Show("삭제실패!", "삭제", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"비정상적 오류 : {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+                
+    }
+}
